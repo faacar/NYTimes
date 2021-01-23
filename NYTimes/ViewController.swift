258d3c02ef5b8150c030d64ed299cd2a439abba5
Ahmet <faruk-16@outlook.com>
2021-01-23 15:47:21 +0300

    Initial Commit

---
+++ NYTimes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYTimes
+//
+//  Created by Ahmet Acar on 23.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
